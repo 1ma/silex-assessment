2c6fadd9c756c72a5cad37185770f0265cf7d24e
Marcel Hernandez <e6990620@gmail.com>
2014-12-02 22:16:56 +0100

    Test driving TwigServiceProvider

--- app/app.php
+++ app/app.php
@@ -4,8 +4,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Silex\Application();
 
+// Services Configuration
+$app->register(
+    new \Silex\Provider\TwigServiceProvider(),
+    array('twig.path' => __DIR__.'/../views')
+);
+
+// Route Definitions
 $app->get('/', function () {
     return 'Welcome to the madness';
+});
+
+$app->get('/hello/{name}', function($name) use ($app) {
+    return $app['twig']->render(
+        'hello.html.twig',
+        array('name' => $name)
+    );
 });
 
 return $app;
