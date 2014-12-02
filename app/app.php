671609b2687b2d6624eb83f2a952765ab8fd6ce6
Marcel Hernandez <e6990620@gmail.com>
2014-12-02 23:06:12 +0100

    Test driving DoctrineServiceProvider

--- app/app.php
+++ app/app.php
@@ -2,24 +2,62 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Silex\Application();
+use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Application();
 
 // Services Configuration
 $app->register(
-    new \Silex\Provider\TwigServiceProvider(),
-    array('twig.path' => __DIR__.'/../views')
+    new DoctrineServiceProvider(),
+    array(
+        'db.options' => array(
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'hospi',
+            'user' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8'
+        )
+    )
+);
+
+$app->register(
+    new TwigServiceProvider(),
+    array('twig.path' => __DIR__ . '/../views')
 );
 
 // Route Definitions
-$app->get('/', function () {
+$app->get('/', function () use ($app) {
     return 'Welcome to the madness';
 });
 
-$app->get('/hello/{name}', function($name) use ($app) {
+$app->get('/hello/{name}', function ($name) use ($app) {
     return $app['twig']->render(
         'hello.html.twig',
         array('name' => $name)
     );
 });
+
+$app->get('/insert', function (Request $request) use ($app) {
+    $email = $request->query->get('email');
+    $password = $request->query->get('password');
+
+    if (null === $email) {
+        return 'email parameter is missing';
+    }
+
+    if (null === $password) {
+        return 'password parameter is missing';
+    }
+
+    $app['db']->insert('users', array('email' => $email, 'password' => $password));
+
+    return 'Successful insert';
+});
+
+$app['debug'] = true;
 
 return $app;
