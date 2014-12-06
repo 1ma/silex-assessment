fbc8f40dc16365e66c3b62bde103f1eb79e742c9
Marcel Hernandez <e6990620@gmail.com>
2014-12-06 13:10:57 +0000

    Enabled Security component, users & login

--- app/app.php
+++ app/app.php
@@ -5,6 +5,8 @@ $app = require_once __DIR__ . '/bootstrap.php';
 use Monolog\Logger;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
+use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,7 +17,28 @@ $app->register(
     new MonologServiceProvider(),
     array(
         'monolog.logfile' => __DIR__ . '/../var/logs/development.log',
-        'monolog.level' => Logger::WARNING
+        'monolog.level' => Logger::DEBUG
+    )
+);
+
+$app->register(new SessionServiceProvider());
+$app->register(
+    new SecurityServiceProvider(),
+    array(
+        'security.firewalls' => array(
+            'main' => array(
+                'pattern' => '^/',
+                'anonymous' => true,
+                'form' => array('login_path' => '/login', 'login_check' => '/login_check'),
+                'logout' => array('logout_path' => '/logout'),
+                // TODO Set custom UserProvider
+                'users' => array('admin@hospi.dev' => array('ROLE_ADMIN', 'nhDr7OyKlXQju+Ge/WKGrPQ9lPBSUFfpK+B1xqx/+8zLZqRNX0+5G1zBQklXUFy86lCpkAofsExlXiorUcKSNQ=='))
+            )
+        ),
+        'security.access_rules' => array(
+            array('^/login$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
+            array('^/homepage', 'ROLE_ADMIN')
+        )
     )
 );
 
@@ -56,16 +79,22 @@ $app->get('/insert', function (Request $request) use ($app) {
     return 'Successful insert';
 });
 
-$app->post('/login', function (Request $request) use ($app) {
-    // TODO log in and redirect to /homepage
+$app->get('/login', function (Request $request) use ($app) {
+    return $app['twig']->render(
+        'login.html.twig',
+        array(
+            'error'         => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+        )
+    );
 });
 
 $app->post('/logout', function (Request $request) use ($app) {
     // TODO log out current user and redirect to /
 });
 
-$app->post('/homepage', function (Request $request) use ($app) {
-    // TODO accept authenticated users only
+$app->get('/homepage', function (Request $request) use ($app) {
+    return "<pre>" . print_r($app['security']->getToken()->getUser(), true) . "</pre>";
 });
 
 return $app;
